5186b2fd5cc51fd6a32312da54c4c0df4a3aee94
Tejanshu <tezz49332@gmail.com>
2024-03-18 20:32:00 +0530

    minor change

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -29,7 +29,7 @@ const UpdateRestaurant = (props) => {
 
         fetchData();
 
-    }, []);
+    }, []);  // this is kept empty
 
     const handleSubmit = async (e) => {
         e.preventDefault();
--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 */
 
 app.use("/", (req,res) => {
-  res.send("server is running");
+  res.send("server is running...");
 });
 
 //To define a route in express, first we have to reference the express instance(object)
